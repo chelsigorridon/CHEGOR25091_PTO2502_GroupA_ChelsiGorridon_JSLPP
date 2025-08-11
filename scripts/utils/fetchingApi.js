e43d176c069c8bb4c93b79de3354d4be12fc4ecf
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-08-11 10:52:20 +0200

    Fetched data from API

--- scripts/utils/fetchingApi.js
+++ scripts/utils/fetchingApi.js
@@ -1,16 +1,30 @@
 
+import { renderTasks } from "../ui/render.js";
+import { loadTasksFromStorage } from "../utils/localStorage.js";
 
-async function fetchTasksFromApi() {
+
+export async function fetchTasksFromApi() {
   try {
     const response = await fetch("https://jsl-kanban-api.vercel.app/");
     if (!response.ok) throw new Error("API error");
-    return await response.json();
+    const tasks = await response.json();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    return tasks;
   } catch (error) {
     console.error("Error fetching tasks:", error);
-    return [];
+    // fallback to tasks in storage or empty array
+    return loadTasksFromStorage();
   }
 }
 
-export const fetchTasksFromApi = fetchTasksFromApi;
-export const saveTasksToStorage = saveTasksToStorage;
-export const loadTasksFromStorage = loadTasksFromStorage;
+export async function loadInitialTasks() {
+  let tasks = loadTasksFromStorage();
+  if (tasks.length === 0) {
+    tasks = await fetchTasksFromApi();
+  }
+  renderTasks(tasks);
+}
+document.addEventListener("DOMContentLoaded", () => {
+  loadInitialTasks();
+  console.log("DOM ready, loading tasks...");
+});
