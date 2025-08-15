4b577f3378b6bcd2ee4270caf66659f3f2947f3c
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-08-15 22:35:14 +0200

    Imported all files to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 import { loadTasksFromStorage } from "./utils/localStorage.js";
 import { clearExistingTasks, renderTasks } from "./ui/render.js";
-import {
-  setupModalCloseHandler,
-  setupNewTaskModalHandler,
-} from "./ui/modalHandlers.js";
+import { setupModalCloseHandler, setupNewTaskModalHandler } from "./ui/modalHandlers.js";
+import { saveTaskBtn, updateTaskOnBoard} from "./ui/savetask.js";
+import './ui/hidenavbar.js'; 
+import './ui/darkmode.js';
+import { loadTasksFromStorageOrAPI,saveTasksToStorage } from "./utils/fetchingApi.js";
+import {setupDeleteTaskBtn} from "./ui/deletetask.js";
+
 
 function initTaskBoard() {
   const tasks = loadTasksFromStorage();
@@ -11,6 +14,13 @@ function initTaskBoard() {
   renderTasks(tasks);
   setupModalCloseHandler();
   setupNewTaskModalHandler();
+  saveTaskBtn();
+  updateTaskOnBoard();
+  loadTasksFromStorageOrAPI();
+  saveTasksToStorage();
+  setupDeleteTaskBtn();
+
+
 }
 
 document.addEventListener("DOMContentLoaded", initTaskBoard);
